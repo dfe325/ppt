61f6673544045fdcc4c9f10b2f87e0d1f38fc25f
Dan Eder <daneder@gmail.com>
2020-05-01 14:06:52 -0400

    ppt and word V1

---
+++ WIP/ecopy.py
@@ -0,0 +1,13 @@
+from docx import Document
+import os
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.save('ecopy.docx')
